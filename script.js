276336e4f55d7e4c4414f34e012ab4fcdcb623c8
Anjosinfo <anjosinfo2025@gmail.com>
2026-02-01 14:04:03 -0300

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-const servicosCarousel = document.getElementById('servicos-carousel');
-document.getElementById('servicos-left').onclick = () => servicosCarousel.scrollBy({ left: -340, behavior: 'smooth' });
-document.getElementById('servicos-right').onclick = () => servicosCarousel.scrollBy({ left: 340, behavior: 'smooth' });
